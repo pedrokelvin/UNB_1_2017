7a9b92692bc69535c0dcb409491f4e98b4640ba7
Pedro Kelvin <pedrokelvin2006@gmail.com>
2017-04-05 16:52:08 -0300

    Modificação na questão 5

--- OO/Atividade 1/Questao5/src/questao5/Questao5.java
+++ OO/Atividade 1/Questao5/src/questao5/Questao5.java
@@ -6,19 +6,29 @@ public class Questao5 {
     
     public static void main(String[] args) {
         
-        Drone drone1;
+        Drone drone1 = new Drone();
+        ControleRemoto c = new ControleRemoto();
         
-        drone1 = new Drone(4, 10, 10, 150, 7, "SD", "X4 mini", "Hubsan");
+        drone1.vel_hor_max = 1;
         
-        drone1.aumentarVelHor(); //Aumentando a velocidade horizontal em 1 m/s
-        drone1.aumentarVelVert(); //Aumentando a velocidade vertical em 1 m/s
-        
-        System.out.println(drone1.vel_hor_max);
-        System.out.println(drone1.vel_vert_max);
-        
-        drone1.diminuirVelHor(); //Diminuindo a velocidade horizontal em 1 m/s
+        c.x = drone1;
         
         System.out.println(drone1.vel_hor_max);
+        
+        c.AumentarVelHor();
+        
+        System.out.println(drone1.vel_hor_max);
+        
+        /*
+        drone1.vel_hor_max = 1;
+        
+        System.out.println(drone1.vel_hor_max);
+        
+        c.AumentarVelHor(drone1);
+        
+        System.out.println(drone1.vel_hor_max);
+        */
+        
+        
     }
-    
 }
