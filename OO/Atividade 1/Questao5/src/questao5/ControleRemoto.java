9c48463fb1b1d51be2443ef3f619ce247dadf9ec
Pedro Kelvin <pedrokelvin2006@gmail.com>
2017-04-05 16:49:24 -0300

    Modificação na questão 5

---
+++ OO/Atividade 1/Questao5/src/questao5/ControleRemoto.java
@@ -0,0 +1,18 @@
+package questao5;
+
+
+public class ControleRemoto {
+   int autonomiaBateria, distanciaMax;
+   Drone x;
+    
+   public void AumentarVelHor(){
+       x.aumentarVelHor();
+   }
+   
+   /* Aqui o controle remeto recebe como parâmetros 
+   public void AumentarVelHor(Drone d){
+       x.aumentarVelHor();
+   }
+   */
+   
+}
