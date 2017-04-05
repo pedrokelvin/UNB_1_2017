db1dfb0e07e150917ea1def26dc2309460d68251
Pedro Kelvin <pedrokelvin2006@gmail.com>
2017-04-05 16:41:00 -0300

    Modificações em questao

--- TEP/URI/2022.cpp
+++ TEP/URI/2022.cpp
@@ -2,8 +2,26 @@
 
 using namespace std;
 
+typedef struct G{
+  int p;
+  double v;
+  string presente;
+
+}gift;
+
 int main(){
 
-  
+  vector<gift> lista;
+  string nome;
+  int i, qtd;
+  float valor;
+
+  cin >> nome >> i;
+
+  while(i){
+    cin >> nome >> valor >> qtd;
+    lista.presente.push_back(nome);
+  }
+
   return 0;
 }
