d5640d4b5e8aacaa44786c5d1ef8961de0987ea0
Pedro Kelvin <pedrokelvin2006@gmail.com>
2017-04-06 09:23:16 -0300

    Resolvendo questão com nova funcionalidade: pair

--- TEP/URI/2022.cpp
+++ TEP/URI/2022.cpp
@@ -2,26 +2,47 @@
 
 using namespace std;
 
-typedef struct G{
-  int p;
-  double v;
-  string presente;
+using is = pair<int, float>;
 
-}gift;
+#define v1 first.first
+#define v2 first.second
+#define v3 second
+
+bool comp(pair<is,char *> x, pair<is,char *> y);
 
 int main(){
 
-  vector<gift> lista;
   string nome;
-  int i, qtd;
-  float valor;
+  char c[100], d;
+  int n, a;
+  float b;
+  vector<pair<is,char *>> v;
 
-  cin >> nome >> i;
+  cin >> nome >> n;
 
-  while(i){
-    cin >> nome >> valor >> qtd;
-    lista.presente.push_back(nome);
+  while(n--){
+    d = getchar();
+    scanf("%[^\n]",c);
+    cin >> b >> a;
+    v.push_back(make_pair(is(a,b),c));
+  }
+
+  sort(v.begin(),v.end(),comp);
+  cout << "Lista de " << nome << endl;
+  for(auto i:v){
+    cout << i.v3;
+    printf(" - R$%.2f\n",i.v2);
   }
 
   return 0;
+}
+
+bool comp(pair<is,char *> x, pair<is,char *> y){
+
+  if(x.v1 == y.v1)
+    return x.v2 < y.v2;
+  else if(x.v2 == y.v2)
+    return x.v3 < y.v3;
+  else
+    return x.v1 > y.v1;
 }
