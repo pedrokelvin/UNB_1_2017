52133bc2a1991a3b610af9c34134f7be50b48c26
Pedro Kelvin <pedrokelvin2006@gmail.com>
2017-04-07 11:32:10 -0300

    Questao resolvida

--- TEP/URI/2022.cpp
+++ TEP/URI/2022.cpp
@@ -2,47 +2,38 @@
 
 using namespace std;
 
-using is = pair<int, float>;
+using is = pair<long long, long long>;
 
 #define v1 first.first
 #define v2 first.second
 #define v3 second
 
-bool comp(pair<is,char *> x, pair<is,char *> y);
-
 int main(){
 
   string nome;
-  char c[100], d;
-  int n, a;
-  float b;
-  vector<pair<is,char *>> v;
+  char d;
+  int n;
+  long long a, b, c;
 
-  cin >> nome >> n;
+  while(cin >> nome >> n){
+    vector<pair<is,string>> v;
 
-  while(n--){
-    d = getchar();
-    scanf("%[^\n]",c);
-    cin >> b >> a;
-    v.push_back(make_pair(is(a,b),c));
-  }
+    while(n--){
+      string gift;
+      getchar();
+      getline(cin,gift);
+      scanf("%lld.%lld %lld",&a,&b,&c);
+      v.push_back(make_pair(is(-c,a*100 + b),gift));
+    }
 
-  sort(v.begin(),v.end(),comp);
-  cout << "Lista de " << nome << endl;
-  for(auto i:v){
-    cout << i.v3;
-    printf(" - R$%.2f\n",i.v2);
+    sort(v.begin(),v.end());
+
+    cout << "Lista de " << nome << endl;
+    for(auto i:v)
+      printf("%s - R$%lld.%02lld\n",i.v3.c_str(), i.v2/100, i.v2%100);
+
+    cout << endl;
   }
 
   return 0;
-}
-
-bool comp(pair<is,char *> x, pair<is,char *> y){
-
-  if(x.v1 == y.v1)
-    return x.v2 < y.v2;
-  else if(x.v2 == y.v2)
-    return x.v3 < y.v3;
-  else
-    return x.v1 > y.v1;
 }
